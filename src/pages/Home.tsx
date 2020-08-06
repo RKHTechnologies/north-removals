4c0122d43e16a1cc933539455aa59e7dd93588b1
RKH <rhysh6@gmail.com>
2020-08-06 22:20:51 +0100

    Initial footer commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -39,6 +39,8 @@ const Home: FC = () => {
         <Section id="Contact" fullwidth> 
             <Contact />
         </Section>
+
+        {/* <Footer /> */}
       </PageBodyContainer>
       
     </>
