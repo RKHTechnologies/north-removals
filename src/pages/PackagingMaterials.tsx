a6eeb6b65cc608c2726fd36aafdbdb492400e0e8
RKH <rhysh6@gmail.com>
2020-08-12 17:27:54 +0100

    continue with packing materials page

--- src/pages/PackagingMaterials.tsx
+++ src/pages/PackagingMaterials.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 import styled from 'styled-components';
-import { colours } from '../Shared/SharedStyles';
+import { colours, SharedSettings } from '../Shared/SharedStyles';
 import Images, { imageLib } from '../Shared/ImageLib';
 
 const Container = styled.div`
@@ -20,18 +20,22 @@ const GridContainer = styled.div`
   padding: 0 50px;
   box-sizing: border-box;
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  grid-gap: 20px;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+  grid-gap: 10px;
+
+  @media(max-width: ${SharedSettings.mobile}) {
+    padding: 0 5px;
+  }
 `;
 
 
 const GridItem = styled.div`
-  width: 100%;
-  height: 300px;
+  max-width: 390px;
+  min-width: 190px;
   background: #e6e6e6;
-  border-radius: 10px;
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  align-items: center;
 `;
 
 interface ImageProps {
@@ -39,11 +43,20 @@ interface ImageProps {
 }
 
 const Image = styled.div`
-  width: 180px;
-  height: 140px;
+  width: 100%;
+  height: 225px;
   background: url(${(p: ImageProps) => Images[p.image]});
   background-size: contain;
   background-repeat: no-repeat;
+  background-position: center;
+`;
+
+const Name = styled.span``;
+
+const Description = styled.span``;
+
+const Price = styled.div`
+
 `;
 
 const PackagingMaterials: FC = () => {
@@ -54,9 +67,13 @@ const PackagingMaterials: FC = () => {
 
         <GridItem>
           <Image image="box" />
+          <Name>Cardboard Box</Name>
+          <Description>Lorem ipsum dolor sit amet et delectus accommodare his consul copiosae legendos</Description>
+          <Price>12.25</Price>
         </GridItem>
 
         <GridItem>
+
         </GridItem>
 
         <GridItem>
