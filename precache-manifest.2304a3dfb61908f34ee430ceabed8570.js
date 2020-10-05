a2da124ee9e7fde1aeb960e1c87ece9042c7b698
RKH Technologies <rhysh6@gmail.com>
2020-10-05 21:16:21 +0100

    Updates

---
+++ precache-manifest.2304a3dfb61908f34ee430ceabed8570.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dad90303bd0df832855af223a708e123",
+    "url": "/north-removals/index.html"
+  },
+  {
+    "revision": "91c0a5b014498629e172",
+    "url": "/north-removals/static/css/main.ef9e514a.chunk.css"
+  },
+  {
+    "revision": "fa019577ad746b29c53a",
+    "url": "/north-removals/static/js/2.0ed7c25e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/north-removals/static/js/2.0ed7c25e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91c0a5b014498629e172",
+    "url": "/north-removals/static/js/main.b2ea0173.chunk.js"
+  },
+  {
+    "revision": "6b596d4b375199752617",
+    "url": "/north-removals/static/js/runtime-main.9f793428.js"
+  },
+  {
+    "revision": "8c8521de0a6ef7b932fad842b51b3450",
+    "url": "/north-removals/static/media/RKHT_logo.8c8521de.png"
+  },
+  {
+    "revision": "10092ccba44a4db3874b8e6d2192c6e0",
+    "url": "/north-removals/static/media/box-tape.10092ccb.jpg"
+  },
+  {
+    "revision": "bd6d79b2358ce0be2c9efbdbc4bfafce",
+    "url": "/north-removals/static/media/box.bd6d79b2.jpg"
+  },
+  {
+    "revision": "b41588f1655b21261b64db04cbe4e8b0",
+    "url": "/north-removals/static/media/cutting-bubblewrap.b41588f1.jpg"
+  },
+  {
+    "revision": "ff89fba13cc9d6501acb0cd81d2b2dec",
+    "url": "/north-removals/static/media/desk.ff89fba1.jpg"
+  },
+  {
+    "revision": "f10884373511ca43afa9e3d254c394b4",
+    "url": "/north-removals/static/media/homeMoves-320.f1088437.jpg"
+  },
+  {
+    "revision": "03e6ad12d62c8576ae9cb0041775a955",
+    "url": "/north-removals/static/media/logo.03e6ad12.png"
+  },
+  {
+    "revision": "2fc9c057adfb4e72ff6f46bb47ac95d6",
+    "url": "/north-removals/static/media/logo_noText.2fc9c057.png"
+  },
+  {
+    "revision": "5a68cab183f6a6a9ad90abb3cf9e8c32",
+    "url": "/north-removals/static/media/packingService.5a68cab1.jpg"
+  },
+  {
+    "revision": "87c62d131347f32969b5c67dbeb4e30c",
+    "url": "/north-removals/static/media/removals.87c62d13.jpg"
+  },
+  {
+    "revision": "9e71be5a0b41c0de8ce55afe97691dfe",
+    "url": "/north-removals/static/media/removalsVan.9e71be5a.jpg"
+  },
+  {
+    "revision": "b76381ca13cc5a47a673d25185179d1c",
+    "url": "/north-removals/static/media/wrapped-sofa.b76381ca.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.da25a9909ab1f6892e6c432f365faba6.js"
+  "/north-removals/precache-manifest.2304a3dfb61908f34ee430ceabed8570.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/north-removals/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
